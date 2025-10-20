3878e7eca8bd129d640418de577c8777cfe1b9d9
Tanki Classic <98331606+TankiClassic25@users.noreply.github.com>
2025-10-20 16:15:48 +0300

    res

---
+++ s/status.js
@@ -0,0 +1,17 @@
+{
+  "maxSupportedAndroidVersion": 2147483647,
+  "minSupportedAndroidVersion": 1572451069,
+  "nodes": {
+    "main.c1": {
+      "endpoint": {
+        "host": "127.0.0.1",
+        "status": "NORMAL",
+        "tcpPorts": [5001],
+        "wsPorts": [0]
+      },
+      "inbattles": 0,
+      "online": 0,
+      "partners": {}
+    }
+  }
+}
